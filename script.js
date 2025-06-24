11d3e45fc48c7960d5e9e9041cc023eb6d77c53d
Jazmine Roberts <jr29786@georgiasouthern.edu>
2025-06-24 18:42:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -40,19 +40,35 @@ function move() {
 
 function updateTileImages() {
   const cupcakeImages = {
-    '2': 'https://cupcake2048.com/img/1.png',
-    '4': 'https://cupcake2048.com/img/2.png',
-    '8': 'https://cupcake2048.com/img/3.png',
-    '16': 'https://cupcake2048.com/img/4.png',
-    '32': 'https://cupcake2048.com/img/5.png',
-    '64': 'https://cupcake2048.com/img/6.png',
-    '128': 'https://cupcake2048.com/img/7.png',
-    '256': 'https://cupcake2048.com/img/8.png',
-    '512': 'https://cupcake2048.com/img/9.png',
-    '1024': 'https://cupcake2048.com/img/10.png',
-    '2048': 'https://cupcake2048.com/img/11.png'
+    '2': 'https://i.imgur.com/dxjd7FB.png',
+    '4': 'https://i.imgur.com/59fuk5l.png',
+    '8': 'https://i.imgur.com/1G6Xsg3.png',
+    '16': 'https://i.imgur.com/YazrZFb.png',
+    '32': 'https://i.imgur.com/YFgEuMn.png',
+    '64': 'https://i.imgur.com/DZG4ANl.png',
+    '128': 'https://i.imgur.com/Cy7o4cE.png',
+    '256': 'https://i.imgur.com/fTSwhht.png',
+    '512': 'https://i.imgur.com/BfY3yZr.png',
+    '1024': 'https://i.imgur.com/ENuHaWD.png',
+    '2048': 'https://i.imgur.com/HfKpOal.png'
   };
 
+  tiles.forEach(tile => {
+    const value = tile.dataset.value;
+    tile.innerHTML = '';
+
+    if (value && cupcakeImages[value]) {
+      const img = document.createElement('img');
+      img.src = cupcakeImages[value];
+      img.alt = value;
+      img.style.width = '100%';
+      img.style.height = '100%';
+      tile.appendChild(img);
+    } else {
+      tile.textContent = value || '';
+    }
+  });
+}
   tiles.forEach(tile => {
     const value = tile.dataset.value;
     tile.innerHTML = '';
