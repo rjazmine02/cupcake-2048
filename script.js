3a5b47d4f5e2bb150f960b8d8825657334798517
Jazmine Roberts <jr29786@georgiasouthern.edu>
2025-06-24 16:21:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,20 +4,21 @@ function moveLeft() {
   for (let row of grid) {
     let values = row.map(tile => tile.textContent).filter(val => val !== '');
 
-    // Merge matching tiles
+    // Merge matching numbers
     for (let i = 0; i < values.length - 1; i++) {
-      if (values[i] === values[i + 1]) {
+      if (values[i] === values[i + 1] && values[i] !== '') {
         values[i] = String(Number(values[i]) * 2);
-        values[i + 1] = '';
+        values[i + 1] = ''; // Clear the merged tile
       }
     }
 
-    // Remove empty and pad the row
+    // Remove blanks again and pad to length 4
     values = values.filter(val => val !== '');
     while (values.length < 4) {
       values.push('');
     }
 
+    // Put values back into the row
     row.forEach((tile, i) => {
       tile.textContent = values[i];
     });
